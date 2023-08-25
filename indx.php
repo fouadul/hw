765c2175853d3931267f1da824044b223e28a149
fouadul <107912267+fouadul@users.noreply.github.com>
2023-08-25 19:59:00 +0600

    Add files via upload

---
+++ indx.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * statement usig
+ * heading tag
+ * br tag
+ */
+echo "<h1> HEllO WORID </h1>";
+echo "<h2>my name is fouadul islam </h2>";
+
+
+
+//numder
+$a = 40;
+$b = 50;
+echo $a + $b;
+
+
+//variable declaration
+
+$user_name = "<h2> fouadul islam</h2>";
+$my_institute = "<h4>chittagong polytechnic institute</h4>";
+ echo $user_name,$my_institute;
+
+
+//concatenation
+$name = "fouad";
+$age = "narsingdi";
+echo 'i am ' . $name .'i am ' . $age .'years old' ;
+
+
+ ?>
